b539db1b309c89077dc918f37aa268a0db5d304e
Ankit Singh <ankitsingh620162@gmail.com>
2025-03-05 23:35:58 +0530

    Update secondary.js

--- views/backend/secondary.js
+++ views/backend/secondary.js
@@ -1,26 +1,21 @@
-const { required } = require("joi");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Secondary = new Schema({
-    user : {
-        type : Schema.Types.ObjectId,
-        ref : "User"
+const SecondarySchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
     },
 
-    message : {
-        type : String,
-        required : true
+    message: {
+        type: String,
+        required: true,
     },
 
-    date : {
-        type : Date,
-        default : Date.now()
-    }
+    date: {
+        type: Date,
+        default: Date.now, // Pass the function reference, not the result of invoking it
+    },
+});
 
-
-})
-
-module.exports = mongoose.model('Secondary', Secondary);
-
-
+module.exports = mongoose.model("Secondary", SecondarySchema);
